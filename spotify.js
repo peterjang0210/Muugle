8f7424dac5795c284715a2b88a8b04447f23d51b
Peter Jang <peter.jang@outlook.com>
2019-01-31 10:05:06 -0500

    spotifyAPI working

--- spotify.js
+++ spotify.js
@@ -1,10 +1,15 @@
-const queryURL = "https://accounts.spotify.com/authorize?client_id=789d8e0f5720439eacbfb9d9c737bfac&redirect_uri=http://localhost:3000/&scope=user-read-private%20user-read-email&response_type=token&state=123";
-
 const spotifyAPI = function () {
+    const index1 = window.location.hash.indexOf("=") + 1;
+    const index2 = window.location.hash.indexOf("&");
+    const accessToken = window.location.hash.substring(index1, index2);
+    
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+        url: "https://api.spotify.com/v1/search?q=Drake&type=artist",
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (response) {
+            console.log(response);
+        }
     })
 }
