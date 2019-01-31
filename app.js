2b205804ba1ce247a815d9acf5bada739f0320f9
Peter Jang <peter.jang@outlook.com>
2019-01-31 11:39:25 -0500

    accepts user input

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-spotifyAPI();
+const searchSpotify = function () {
+    const userInput = $("#searchBar").val();
+    const trackName = userInput.substring(0, userInput.indexOf(","));
+    const artistName = userInput.substring(userInput.indexOf(",") + 1);
+    spotifyAPI(trackName, artistName);
+}
+
+$("#searchBtn").on("click", searchSpotify);
