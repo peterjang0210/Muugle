20343fd8c48ddb5971871342057eb9f0f3bc1a2d
Peter Jang <peter.jang@outlook.com>
2019-01-31 10:56:55 -0500

    made spotifyAPI URL dynamic

--- spotify.js
+++ spotify.js
@@ -1,10 +1,21 @@
-const spotifyAPI = function () {
+const spotifyAPI = function (track, artist) {
     const index1 = window.location.hash.indexOf("=") + 1;
     const index2 = window.location.hash.indexOf("&");
     const accessToken = window.location.hash.substring(index1, index2);
-    
+    let queryURL = "";
+
+    if(track !== "" && artist === ""){
+        queryURL = `https://api.spotify.com/v1/search?q=${track}&type=track`;
+    }
+    else if(track === "" && artist !== ""){
+        queryURL = `https://api.spotify.com/v1/search?q=${artist}&type=artist`;
+    }
+    else{
+        queryURL = `https://api.spotify.com/v1/search?q=${track}&type=track,artist`;
+    }
+
     $.ajax({
-        url: "https://api.spotify.com/v1/search?q=Drake&type=artist",
+        url: queryURL,
         headers: {
             'Authorization': 'Bearer ' + accessToken
         },
