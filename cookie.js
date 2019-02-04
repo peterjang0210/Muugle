d34f83a86d7792bcf5e76ba11889baab34c80a51
Andrew Linck <alinck@purdue.edu>
2019-02-04 09:38:09 -0500

    Update cookie.js
    syntax spacing

--- cookie.js
+++ cookie.js
@@ -1,5 +1,4 @@
-
-    const trackHistory = []
+const trackHistory = []
 const cookieStore = function (trackID) {
     // everytime that we click track id it has to push to array
     trackHistory.push(trackID);
