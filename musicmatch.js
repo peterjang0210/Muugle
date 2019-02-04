990cc2bddfe14874522a60aca851a49e1a41e53e
Haoyuan Lin <specialemon@gmail.com>
2019-02-04 12:02:45 -0500

    trimmed

--- musicmatch.js
+++ musicmatch.js
@@ -12,49 +12,43 @@ const lyricsBlock = function (lyricsBody) {
     console.log(blockTemplate);
 }
 
-
-const infoPull = function (name) {
-
-    //temp testing url
-    const domainURL = `https://api.genius.com`;
-    const trackListEP = `/search?q=${name}&access_token=h-ekdFJHN-7FvdhejCym_JwHF96ugsaz41cHDfwAQBrtkHqLPV9NepE0aAnPPalu`;
-
+const infoRefine = function (songName, artistName) {
     $.ajax({
-        url: `${domainURL}${trackListEP}`,
+        url: `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=bc5ef36457edb15455aad8c84e027791&artist=${artistName}&track=${songName}&format=json`,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        const dataStart = response.response.hits[0].result;
-        //get meta
-        const songName = dataStart.title_with_featured;
-        const artistName = dataStart.primary_artist.name;
-        console.log(artistName);
-        console.log(songName);
+        const albumName = response.track.album.title;
+        //create meta block
+        metaBlock(songName, artistName, albumName);
+    })
 
-        //get album
-        $.ajax({
-            url: `http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=bc5ef36457edb15455aad8c84e027791&artist=${artistName}&track=${songName}&format=json`,
-            method: "GET"
-        }).then(function(response){
-            console.log(response);
-            const albumName = response.track.album.title;
-            //create meta block
-            metaBlock(songName, artistName, albumName);
-        })
 
-        
-        //get lyrics
-        $.ajax({
-            url: `https://api.lyrics.ovh/v1/${artistName}/${songName}`,
-            method: "GET"
-        }).then(function (response) {
-            //create lyrics block
-            lyricsBlock(response.lyrics);
-        })
+    //get lyrics
+    $.ajax({
+        url: `https://api.lyrics.ovh/v1/${artistName}/${songName}`,
+        method: "GET"
+    }).then(function (response) {
+        //create lyrics block
+        const lyrics = decodeURI(encodeURI(response.lyrics).replace(/%0A/g, "<br/>"));
+        lyricsBlock(`${lyrics}`);
     })
 }
 
-//testing area
+
+const infoPull = function (song, artist) {
+
+
+        //get meta
+        const songName = song.replace(/’/g, "'");
+        const artistName = artist.replace(/’/g, "'");
+
+        //get album and lyrics info
+        infoRefine(songName, artistName);
+    })
+}
+
+//testing and legacy area
 
 // const render = function (event) {
 //     event.preventDefault();
@@ -68,7 +62,40 @@ const infoPull = function (name) {
 //     $("#metaLyrics").empty();
 // }
 
-infoPull("never gonna give you up");
+// infoPull("sweet child");
 
 // $("#testButton").on("click", testEmpty);
 
+
+//legacy genius code
+// const infoPull = function (song, artist) {
+
+//     //temp testing url
+//     const domainURL = `https://api.genius.com`;
+//     const trackListEP = `/search?q=${song}&access_token=h-ekdFJHN-7FvdhejCym_JwHF96ugsaz41cHDfwAQBrtkHqLPV9NepE0aAnPPalu`;
+
+//     $.ajax({
+//         url: `${domainURL}${trackListEP}`,
+//         method: "GET"
+//     }).then(function (response) {
+//         let index = 0;
+//         let dataStart = response.response.hits[index].result;
+//         const parseStart = response.response.hits;
+//         for(i=0; i< parseStart.length; i++) {
+//             if(song.toUpperCase() == parseStart[i].title.toUpperCase() && artist.toUpperCase() == artist) {
+//                 index = i;
+//             }
+//         }
+
+//         //get meta
+//         const songName = dataStart.title.replace(/’/g, "'");
+//         const artistName = dataStart.primary_artist.name.replace(/’/g, "'");
+//         // const songName = song;
+//         // const artistName = artist;
+
+
+
+//         //get album
+//         infoRefine("", artistName)
+//     })
+// }
