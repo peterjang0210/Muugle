82c0e72ff2e68e47ab8d4b703a720a4b38959f14
Andrew Linck <alinck@purdue.edu>
2019-02-05 10:04:03 -0500

    coookie update

--- cookie.js
+++ cookie.js
@@ -1,8 +1,17 @@
-const trackHistory = []
+// Cookies are always strings and must be converted to arrays in order to be manipulated in our javscript, which is done using the JSON.parse
+// We then have to JSON.stringify to convert it back to a string otherwise it will not be stored in the cookie
+
+function getCookie(name) {
+    var c = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return c ? c[2] : '';
+}
+
+let cookie = getCookie('trackhistory');
+// JSON.parse turns the string into an array only if the cookie is not empty, otherwise it creates an empty array
+cookie = cookie === "" ? [] : JSON.parse(cookie)
+
 const cookieStore = function (trackID) {
-    // everytime that we click track id it has to push to array
-    trackHistory.push(trackID);
-    console.log(trackHistory);
-    
-    document.cookie = `trackhistory=[${trackHistory}]`
+    cookie.push(trackID);
+// JSON.stringify truns the array to a string so that it may be stored in the cookie
+    document.cookie = `trackhistory=${JSON.stringify(cookie)}`
 }
